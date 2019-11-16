bd26a078055e1f1b02867e0c84ed97879cb13669
junyakaneko <junya@mpsamurai.org>
2019-11-17 00:45:30 +0900

    Changed python version to 3.7

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose-alpine-run-on-x64.yml build
-docker push mpsamurai/neochi:20191201-x64
+#docker-compose -f docker-compose-alpine-run-on-x64.yml build
+#docker push mpsamurai/neochi:20191201-x64
 
 docker-compose -f docker-compose-raspbian-run-on-x64.yml build
 docker push mpsamurai/neochi:20191201-raspbian
