85112957b1b039d05c8325b056fdf096bc4c86e0
Junya Kaneko <junya@mpsamurai.org>
2019-11-16 16:12:58 +0900

    Modified files for dockerize.

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose-ubuntu-run-on-x64.yml build
+docker-compose -f docker-compose-alpine-run-on-x64.yml build
 docker push mpsamurai/neochi:20191201-x64
 
 docker-compose -f docker-compose-raspbian-run-on-x64.yml build
