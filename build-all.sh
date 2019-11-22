675f1a4ebffa1bbe6223db2367a253f1803ee1f1
Junya Kaneko <junya@mpsamurai.org>
2019-11-22 22:26:42 +0900

    Temporary commits.

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@
 #docker push mpsamurai/neochi:20191201-x64
 
 docker pull mpsamurai/neochi:20191201-raspbian-base
-docker build -t mpsamurai/neochi:20191201-raspbian-base -f Dockerfile-raspbian-base .
+docker build -t mpsamurai/neochi:20191201-raspbian-base -f raspbian-cv2 .
 docker push mpsamurai/neochi:20191201-raspbian-base
 
 docker-compose -f docker-compose-raspbian-run-on-x64.yml build
