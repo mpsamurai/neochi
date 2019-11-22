bccd930fb6fdcdadad91746680d1e0849db8969d
junyakaneko <junya@mpsamurai.org>
2019-11-22 23:01:01 +0900

    Changed buster to stretch.

--- build-all.sh
+++ build-all.sh
@@ -3,9 +3,11 @@
 #docker-compose -f docker-compose-alpine-run-on-x64.yml build
 #docker push mpsamurai/neochi:20191201-x64
 
-docker pull mpsamurai/neochi:20191201-raspbian-base
-docker build -t mpsamurai/neochi:20191201-raspbian-base -f raspbian-cv2 .
-docker push mpsamurai/neochi:20191201-raspbian-base
+docker build -t mpsamurai/neochi:raspbian-python38 -f Dockerfile-raspbian-python38 .
+docker push mpsamurai/neochi:raspbian-python38
 
-docker-compose -f docker-compose-raspbian-run-on-x64.yml build
-docker push mpsamurai/neochi:20191201-raspbian
+docker build -t mpsamurai/neochi:raspbian-cv2 -f Dockerfile-raspbian-cv2 .
+docker push mpsamurai/neochi:raspbian-cv2
+
+docker build -t mpsamurai/neochi:raspbian -f Dockerfile-raspbian .
+docker push mpsamurai/neochi:raspbian
