58556e3cbe128606325bba4c89c9af316522db15
junyakaneko <junya@mpsamurai.org>
2019-11-24 12:33:02 +0900

    Modified to accept options.

--- build-all.sh
+++ build-all.sh
@@ -1,22 +1,45 @@
 #!/usr/bin/env bash
 
-#docker-compose -f docker-compose-alpine-run-on-x64.yml build
-#docker push mpsamurai/neochi:20191201-x64
+usage() {
+  echo "Usage: $0: [-b branch name] [-p number of processors]" 2>&1
+  exit 1
+}
 
-#echo "========================================================="
-#echo "Build mpsamurai/neochi:raspbian-python38"
-#echo "========================================================="
-#docker build -t mpsamurai/neochi:raspbian-python38 --build-arg n_proc=94 -f Dockerfile-raspbian-python38 .
-#docker push mpsamurai/neochi:raspbian-python38
 
-#echo "========================================================="
-#echo "Build mpsamurai/neochi:raspbian-cv2"
-#echo "========================================================="
-#docker build -t mpsamurai/neochi:raspbian-cv2 --build-arg n_proc=94 -f Dockerfile-raspbian-cv2 .
-#docker push mpsamurai/neochi:raspbian-cv2
+while getopts ":b:p:" o
+do
+  case $o in
+  b)
+    b=${OPTARG}
+    ;;
+  p)
+    p=${OPTARG}
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+
+if [ -z "$b" ] || [ -z "$p" ]
+then
+  usage
+fi
+
+echo "========================================================="
+echo "Build mpsamurai/neochi:raspbian-python38"
+echo "========================================================="
+docker build -t mpsamurai/neochi:raspbian-python38 --build-arg n_proc="$p" -f Dockerfile-raspbian-python38 .
+docker push mpsamurai/neochi:"$b"-python38
+
+echo "========================================================="
+echo "Build mpsamurai/neochi:raspbian-cv2"
+echo "========================================================="
+docker build -t mpsamurai/neochi:raspbian-cv2 --build-arg n_proc="$p" -f Dockerfile-raspbian-cv2 .
+docker push mpsamurai/neochi:"$b"-cv2
 
 echo "========================================================="
 echo "Build mpsamurai/neochi:raspbian"
 echo "========================================================="
-docker build -t mpsamurai/neochi:raspbian --build-arg n_proc=94 -f Dockerfile-raspbian .
-docker push mpsamurai/neochi:raspbian
+docker build -t mpsamurai/neochi:raspbian --build-arg n_proc="$p" -f Dockerfile-raspbian .
+docker push mpsamurai/neochi:"$b"
