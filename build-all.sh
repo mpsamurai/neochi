e41983700fb2f4ba0d6f55a2eaba07a136000968
junyakaneko <junya@mpsamurai.org>
2019-11-30 03:12:37 +0900

    Tried to install tensorflow.

--- build-all.sh
+++ build-all.sh
@@ -26,17 +26,23 @@ then
   usage
 fi
 
-echo "========================================================="
-echo "Build mpsamurai/neochi:raspbian-python38"
-echo "========================================================="
-docker build -t mpsamurai/neochi:raspbian-python38 --build-arg n_proc="$p" -f Dockerfile-raspbian-python38 .
-docker push mpsamurai/neochi:"$b"-python38
-
-echo "========================================================="
-echo "Build mpsamurai/neochi:raspbian-cv2"
-echo "========================================================="
-docker build -t mpsamurai/neochi:raspbian-cv2 --build-arg n_proc="$p" -f Dockerfile-raspbian-cv2 .
-docker push mpsamurai/neochi:"$b"-cv2
+#echo "========================================================="
+#echo "Build mpsamurai/neochi:raspbian-python38"
+#echo "========================================================="
+#docker build -t mpsamurai/neochi:raspbian-python38 --build-arg n_proc="$p" -f Dockerfile-raspbian-python38 .
+#docker push mpsamurai/neochi:"$b"-python38
+#
+#echo "========================================================="
+#echo "Build mpsamurai/neochi:raspbian-cv2"
+#echo "========================================================="
+#docker build -t mpsamurai/neochi:raspbian-cv2 --build-arg n_proc="$p" -f Dockerfile-raspbian-cv2 .
+#docker push mpsamurai/neochi:"$b"-cv2
+#
+#echo "========================================================="
+#echo "Build mpsamurai/neochi:raspbian"
+#echo "========================================================="
+#docker build -t mpsamurai/neochi:raspbian --build-arg n_proc="$p" -f Dockerfile-raspbian .
+#docker push mpsamurai/neochi:"$b"
 
 echo "========================================================="
 echo "Build mpsamurai/neochi:raspbian"
